5e83459a55849848e350eab36a06ef0fc7d6f999
timeXI <124089478+timeXI@users.noreply.github.com>
2023-02-03 15:03:52 +0400

    test

--- counter/ViewController.swift
+++ counter/ViewController.swift
@@ -9,6 +9,18 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var label: UILabel!
+    var count = 0
+    
+    @IBAction func button(_ sender: Any) {
+        count += 1
+        label.text = "Значение счётчика: \(count)"
+    }
+    // кнопка, которая обнуляет счет
+    @IBAction func reFresh(_ sender: Any) {
+        count = 0
+        label.text = "Значение счётчика: \(count)"
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
